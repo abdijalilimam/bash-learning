40ca9207eda0b66d90c7198cf132b16e1267cfb3
abdijalilimam <abdijalilimam@gmail.com>
2026-01-08 18:34:47 -0500

    Folder for new bash learning

---
+++ day01.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#variables and output
+
+name="Abdijalil"
+today=$(date)
+os="LINUX"
+current_dir=$(pwd)
+
+
+echo "===== SYSTEM INFO ====="
+echo "User: $name"
+echo "OS: $os"
+echo "$today"
+echo "$current_dir"
+echo "======================="
+
+
